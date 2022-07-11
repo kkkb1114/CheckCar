5a53f3453ac596946eb3e4241a4c2bebe9e35a0a
kkkb1114 <kikkb1114@gmail.com>
2022-07-12 06:58:12 +0900

    2022.07.12
    <완료>
    1. 레이아웃 대략 붙인 상태이다.
    
    <해야할일>
    1. 레이아웃을 붙였으나 몇몇 컬러, 문자 값이 맞지 않아 카톡 참고하여 수정하기
    2. 임시 작성 버튼 클릭시 항목 선택 화면 프래그먼트 전환 적용해야함

--- app/src/main/java/com/example/checkcar/Carbook/CarbookMainMaintenanceOthersPageFragment.java
+++ app/src/main/java/com/example/checkcar/Carbook/CarbookMainMaintenanceOthersPageFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,22 +12,33 @@ import androidx.fragment.app.Fragment;
 
 import com.example.checkcar.R;
 
-public class CarbookMainMaintenanceOthersPageFragment extends Fragment {
+public class CarbookMainMaintenanceOthersPageFragment extends Fragment implements View.OnClickListener{
 
+    TextView tv_test;
 
     public CarbookMainMaintenanceOthersPageFragment() {
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_carbookmain_maintenanceothers, container, false);
-
+        setView(view);
         return view;
+    }
+
+    public void setView(View view){
+        tv_test = view.findViewById(R.id.tv_test);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.tv_test:
+
+                break;
+
+        }
     }
 }
